4de23860b8e2a952921d2211edf711afd5f3e6b1
Yusuf Fadairo <yusuf.kami@gmail.com>
2022-05-05 16:10:42 -0400

    serve swagger ui with express

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { getLocationWeather } from './routes/weather';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+import path from 'path';
 
 dotenv.config();
 
 const app: Express = express()
 
 const PORT = process.env.PORT
+
+const swaggerDocument = yaml.load(path.resolve('./swagger.yaml'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/api/v2/weather/:location', getLocationWeather);
 
